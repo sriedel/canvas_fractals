98785a8eeaf9e3e926512a6d397e92ba4f599115
Sven Riedel <sr@gimp.org>
2011-08-21 14:12:43 +0200

    Control the iterations with the mouse wheel

--- julia_controller.js
+++ julia_controller.js
@@ -1,25 +1,31 @@
 var JuliaController = function( domId, recomputeFunc ) {
   this.element = document.getElementById( domId );
 
-  this.element.addEventListener( "mousemove", this.mouseMoved, null );
-  this.oldMouseX = null;
-  this.oldMouseY = null;
+  this.element.addEventListener( "mousemove", this.mouseMoved, false );
+  this.element.addEventListener( "DOMMouseScroll", this.mouseMoved, false );
 
   JuliaController.recomputeFunc = recomputeFunc;
+  JuliaController.iterations = 5;
 };
+
+JuliaController.adjustIterations = 
+  function( delta ) {
+    JuliaController.iterations += delta;
+    if( JuliaController.iterations < 1 ) {
+      JuliaController.iterations = 1;
+    } else if ( JuliaController.iterations > 50 ) {
+      JuliaController.iterations = 50;
+    }
+  }
 
 JuliaController.prototype.mouseMoved = 
   function( event ) {
-    if( event.clientX == this.oldMouseX &&
-        event.clientY == this.oldMouseY ) {
-      return;
-    }
-
     var relativeX = event.clientX - event.target.offsetLeft;
     var relativeY = event.clientY - event.target.offsetTop;
 
-    JuliaController.recomputeFunc( relativeX, relativeY );
+    JuliaController.recomputeFunc( relativeX, relativeY, event.detail );
 
     this.oldMouseX = event.clientX;
     this.oldMouseY = event.clientY;
   }
+
